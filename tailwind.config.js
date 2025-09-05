4c9798edbc220fd8d1aea0fbc21dc26b0eea0ea0
Pratik Aher <aherpratik313@gmail.com>
2025-09-05 17:44:08 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,16 @@
-
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        farmerGreen: "#2e7d32",
+        farmerYellow: "#fbc02d",
+      },
+    },
+  },
+  plugins: [],
+};
