a00125d71b6f9325a8ffc50104898be399a43462
Pratik Aher <aherpratik313@gmail.com>
2025-09-05 17:39:15 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
-// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import "./index.css"; // Tailwind CSS styles
 import App from "./App";
-import "./index.css"; // Tailwind + global styles
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
